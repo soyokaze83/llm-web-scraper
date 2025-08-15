e1b17c7b3bfabed145d2ca46ab7292149779262e
Vincent <vincent@LAPTOP-0I82CC8G.localdomain>
2025-08-15 20:43:55 +0700

    fix: import webscraper, rename var to user_input

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import asyncio
 
-from scraper.webscraper import WebScraper
+from webscraper import WebScraper
 
 
 async def main():
     url_input = input("Enter link: ")
-    nl_input = input("What would you like to scrape?\n")
+    user_input = input("What would you like to scrape?\n")
 
     async with WebScraper(url_input, headless=True) as scraper:
         full_html = await scraper.get_content(selector="input")
